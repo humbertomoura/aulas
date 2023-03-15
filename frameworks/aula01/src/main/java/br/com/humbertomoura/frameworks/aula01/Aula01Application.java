60a238161b8d0873455e650c77a4c7743b8eb022
Humberto Moura <humberto@humbertomoura.com.br>
2023-03-15 20:03:18 +0000

    Project start

---
+++ frameworks/aula01/src/main/java/br/com/humbertomoura/frameworks/aula01/Aula01Application.java
@@ -0,0 +1,15 @@
+package br.com.humbertomoura.frameworks.aula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01Application.class, args);
+		System.out.println("Hello World!");
+
+	}
+
+}
